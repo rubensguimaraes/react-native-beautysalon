c9fc73464e20f5bab031679f172111b67d05e769
Rubens Guimarães <guimaraes.rubens@gmail.com>
2020-07-12 21:11:16 -0300

    Roteamento no React Native: Rtona inicial e Estilização das rotas

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import { View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import Background from '~/components/Background';
 
 export default function Dashboard() {
-  return <View />;
+  return <Background />;
 }
+
+Dashboard.navigationOptions = {
+  // Definição do título de ícone da aba
+  tabBarLabel: 'Agendamentos',
+  // Foi criada uma função abaixo, pois a color vira do react-navigation
+  // O parâmetro tintColor recebido será transferido para o ícone e a cord
+  // será preenchida de forma automática
+  tabBarIcon: ({tintColor}) => (
+    <Icon name="event" size={20} color={tintColor} />
+  ),
+};
