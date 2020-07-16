aa01c046a31b7ea206636b0b2eb556ae93d73d23
Rubens Guimarães <guimaraes.rubens@gmail.com>
2020-07-15 23:55:40 -0300

    Atualização de perfil e Logout

--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -7,17 +7,15 @@ import {updateProfileSuccess, updateProfilefailure} from './actions';
 
 export function* updateProfile({payload}) {
   try {
-    const {name, email, avatar_id, ...rest} = payload.data;
+    const {name, email, ...rest} = payload.data;
 
     const profile = {
       name,
       email,
-      avatar_id,
       ...(rest.oldPassword ? rest : {}),
     };
 
     const response = yield call(api.put, 'users', profile);
-
     Alert.alert('Sucesso!', 'Perfil atualizado com sucesso!');
 
     yield put(updateProfileSuccess(response.data));
