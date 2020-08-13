0459156d403e274f0c565fb4ada6a4b6c33fda55
Rubens Guimarães <guimaraes.rubens@gmail.com>
2020-08-13 19:12:20 -0300

    Atualizações para deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const IP_ADDRESS = 'http://10.0.2.2:3333';
+const IP_ADDRESS = 'https://api-beautysalon.rubensguimaraes.com';
+// const IP_ADDRESS = 'http://10.0.2.2:3333';
 // const IP_EMULADOR_IOS = 'localhost:3333';
 
 const api = axios.create({
